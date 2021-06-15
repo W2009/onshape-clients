75fe23b4fcac51734372cd8901b02545b5475fd0
w2009 <tomasz.stefanczyk@gmail.com>
2021-06-15 11:32:59 +0200

    test

--- python/onshape_client/client.py
+++ python/onshape_client/client.py
@@ -154,7 +154,7 @@ class Client:
             (that includes the authorization code!)
         """
         
-        prnt("FILE:"+keys_file)
+        print("FILE:"+keys_file)
         if Client.singleton_instance:
             warnings.warn(
                 "A Client was already created so this will create another and override it. Please use "
